dc35c90bd0a3c5b4a80cbe1a92001e2fd078ce65
Marcus Sena <mens.eai20@uea.edu.br>
2024-09-14 07:09:20 -0400

    Adding Module in Python

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from google_module import gemini_api
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Flask!'
+    return gemini_api.hello_from_gemini()
 
 if __name__ == '__main__':
     app.run(debug=True)
